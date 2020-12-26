09cc9930621e29052cd5ac16ceb44b851030c588
Orkhan Mirzayev <mirzesoyo94@gmail.com>
2020-12-26 14:48:24 +0400

    initial commit

--- src/main/java/controller/ClassController.java
+++ src/main/java/controller/ClassController.java
@@ -1,28 +1,48 @@
 package main.java.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import main.java.model.ClassRoom;
 import main.java.src.main.Home;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ClassController implements Initializable {
+    @FXML
+    private TableView<ClassRoom> tv = new TableView<>();
+    @FXML
+    private TableColumn<ClassRoom, Integer> id;
+    @FXML
+    private TableColumn<ClassRoom, String> name;
+    @FXML
+    private TableColumn<ClassRoom, String> lesson;
+    @FXML
+    private TableColumn<ClassRoom, String> teacher;
+    @FXML
+    private TableColumn<ClassRoom, Double> time;
+    @FXML
+    private TextField txtId;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtLesson;
+    @FXML
+    private TextField txtTeacher;
+    @FXML
+    private TextField txtTime;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
-    public void openToClassA(){
-        Home.changeScreen("/classa.fxml");
-    }
-    public void openToClassB(){
-        Home.changeScreen("/classb.fxml");
 
-    }
-    public void openToClassC(){
-        Home.changeScreen("/classc.fxml");
-
-    }
-    public void backToHome(){
+    public void backToHome() {
         Home.changeScreen("/home.fxml");
     }
 }
--- src/main/java/model/ClassRoom.java
+++ src/main/java/model/ClassRoom.java
@@ -1,4 +1,60 @@
 package main.java.model;
 
 public class ClassRoom {
+    private int id;
+    private String name;
+    private String lesson;
+    private String teacher;
+    private Double time;
+
+    public ClassRoom() {
+    }
+
+    public ClassRoom(int id, String name, String lesson, String teacher, Double time) {
+        this.id = id;
+        this.name = name;
+        this.lesson = lesson;
+        this.teacher = teacher;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(String lesson) {
+        this.lesson = lesson;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public Double getTime() {
+        return time;
+    }
+
+    public void setTime(Double time) {
+        this.time = time;
+    }
 }
